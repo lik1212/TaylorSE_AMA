f03e72394f7515a6b5af4624f4cc54856fca419b
brandalik <brandalik@eit.uni-kl.de>
2018-07-19 15:32:31 +0200

    Damoe file adjustments.

---
+++ Demo_TaylorSE_AMA_main.m
@@ -0,0 +1,38 @@
+%% Demostration file for Taylor SE with the Augmented Matrix approach
+%  As input you need z_all_data, z_all_flag, LineInfo and U_eva, check
+%  the input data description for more details. 
+%
+% Author(s):    R. Brandalik
+%               D. Henschel
+%               J. Tu
+%               P. Gassler
+%
+% Contact: brandalikrobert@gmail.com, brandalik@eit.uni-kl.de
+%
+% Special thanks go to the entire TUK ESEM team.
+%
+% Parts of the work were the result of the project CheapFlex, sponsored by
+% the German Federal Ministry of Economic Affairs and Energy as part of the
+% 6th Energy Research Programme of the German Federal Government. 
+
+%% Clear start
+
+path(pathdef); clear; close; clc
+
+%% Path preperation
+
+addpath([pwd,'\Subfunctions']);  % Add subfunction path
+
+%% Load Demo Data
+
+load([pwd,'\Demo_Data\Demo_Data_v1.mat']); 
+
+%% Inputs for State Estimation (can be extended with Inputs)
+
+Inputs_SE.U_eva = 400/sqrt(3); % Voltage of linearization evaluation (eva)
+
+%% Main estimation alfo
+
+tic
+[x_hat, z_hat, z_hat_full] = TaylorSE_AMA(z_all_data, z_all_flag, LineInfo, Inputs_SE);
+toc
