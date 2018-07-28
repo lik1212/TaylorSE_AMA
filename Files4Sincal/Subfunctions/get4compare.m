a78b6c54566fb588b8155987420428258b8c6cab
brandalik <brandalik@eit.uni-kl.de>
2018-07-28 14:54:19 +0200

    Adjusted demo data and preperation for deleting the get4compare function.

--- Files4Sincal/Subfunctions/get4compare.m
+++ Files4Sincal/Subfunctions/get4compare.m
@@ -1,4 +1,4 @@
-function [BranchRes_all_estim, BranchRes_all_exakt, NodeRes_all_estim, NodeRes_all_exakt] = get4compare(Input_Prep, z_hat_full, Y_L1L2L3)
+function [BranchRes_all_estim, NodeRes_all_estim] = get4compare(Input_Prep, z_hat_full, Y_L1L2L3)
 %GET4COMPARE Prepare the estimated data to compare with exakt data (the
 %exakt data is the load flow result used as input data for SE)
 %
@@ -14,25 +14,12 @@ function [BranchRes_all_estim, BranchRes_all_exakt, NodeRes_all_estim, NodeRes_a
 %% Load exakt values to compare with estimated
 
 if Input_Prep.with_TR == false % Data with or wo TR
-    NodeRes_PathName   = [Input_Prep.LF_Res_Path, Input_Prep.NodeRes_Name      ];
-    BranchRes_PathName = [Input_Prep.LF_Res_Path, Input_Prep.BranchRes_Name    ];
     SimDetails         = [Input_Prep.LF_Res_Path, Input_Prep.Simulation_Details];
 else
-    NodeRes_PathName   = [Input_Prep.LF_Res_Path, Input_Prep.      NodeRes_Name(1 : end - 4) ,'_wo_TR.mat'];
-    BranchRes_PathName = [Input_Prep.LF_Res_Path, Input_Prep.    BranchRes_Name(1 : end - 4) ,'_wo_TR.mat'];
     SimDetails         = [Input_Prep.LF_Res_Path, Input_Prep.Simulation_Details(1 : end - 4) ,'_wo_TR.mat'];
 end
-
 load(SimDetails        , 'SimDetails'   );
-load(NodeRes_PathName  , 'NodeRes_all'  );
-load(BranchRes_PathName, 'BranchRes_all');
 
-% Maybe this sorting is not nessecary, but just in case:
-NodeRes_all = sortrows(NodeRes_all,'Node_ID','ascend'); %#ok Data was loaded
-NodeRes_all = sortrows(NodeRes_all,'ResTime','ascend');
-
-NodeRes_all_exakt   = NodeRes_all       ;
-BranchRes_all_exakt = BranchRes_all     ;
 SinInfo             = SimDetails.SinInfo;
 
 clear NodeRes_all BranchRes_all SimDetails
